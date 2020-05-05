62895e43c7cae1b7a84cba50130ceac655fbd4a4
sai sasank <saisasank001@gmail.com>
2020-05-05 12:27:11 +0530

    criteria added ts

--- src/app/criteria/criteria.component.ts
+++ src/app/criteria/criteria.component.ts
@@ -14,7 +14,7 @@ export class CriteriaComponent implements OnInit {
 
   ngOnInit() {
     this.criteriaForm = this.formBuilder.group({
-      state: ['',[Validators.required]],
+      state: ['',[]],
       data_source:['',[Validators.required]],
       from_living_area_sf:['',[Validators.required]],
       to_living_area_sf:['',[Validators.required]],
@@ -34,5 +34,128 @@ export class CriteriaComponent implements OnInit {
     }); 
 
   }
+
+  getRecordsCount(data) {
+    // this.loaderService.showLoader()
+    // this.records = this.loaderService.showProductsLoader();
+    let json = {
+        // session_id: this.userService.getSessionId(),
+        state:data.state
+
+    };
+
+    // this.httpService.postApi(json, 'Homemaster/test').subscribe(res => {
+    //     console.log(res);
+    //     this.loaderService.hideLoader()
+
+    //     if (res) {
+    //         try {
+    //             if (!this.cqdqtApiExceptionService.checkApiException(res)) {
+    //                 this.records = res['results'][0]['total_records'];
+
+    //             } else{
+    //                 this.records=0;
+    //             }
+    //         }catch(e){
+    //             this.records=0;
+    //         }
+
+    //     }else{
+    //         this.records=0;
+    //         let modalId = this.utilService.generateModalId();
+    //         this.dataService.changeMessage(JSON.stringify({
+    //             type: 'modal',
+    //             requestId: modalId,
+    //             modalType: "error",
+    //             buttonText: "Okay",
+    //             message: "There is a fault in fetching records count, Please try again."
+    //         }));
+    //         this.dataService.currentMessage.subscribe((message: any) => {
+    //             if (message != 'default message') {
+    //                 message = JSON.parse(message);
+    //                 if (message['type'] == 'return' && message['returnId'] == modalId) {
+    //                     if(message['handler']=='ok'){
+    //                         this.utilService.closeModal();
+    //                     }
+    //                     // if(message['handler']=='cancel'){}
+    //                 }
+    //             }
+    //         })
+    //     }
+    // }, err => {
+    //     // this.records=0;
+    //     // this.loaderService.hideLoader();
+    //     // this.utilService.showNetworkErrorModal();
+    // });
+
+
+}
+
+clearAll(){
+    this.homeLifeMasterListForm.reset();
+    this.homeLifeMasterListForm.patchValue({
+        ali: "",  // example
+      })
+    this.getRecordsCount(this.homeLifeMasterListForm.value);
+}
+
+downloadRecords() {
+    try {
+        let data = this.homeLifeMasterListForm.value;
+        let json = {
+            // session_id: this.userService.getSessionId(),
+            state:data.state
+            // max_records: this.downloadLimit, // adding download limit
+            // start_at: "",
+        };
+        console.log(data);
+        // this.loaderService.showLoader();
+        // this.httpService.postApi(json, 'Masterlist/test1').subscribe(res => {
+        //     console.log(res)
+        //     this.loaderService.hideLoader();
+        //     if (!res.success){
+        //         let modalId = this.utilService.generateModalId();
+        //         this.dataService.changeMessage(JSON.stringify({
+        //             type: 'modal',
+        //             requestId: modalId,
+        //             modalType: "error",
+        //             buttonText: "Okay",
+        //             message: "There is a fault in downloading records count, Please try again."
+        //         }));
+        //         this.dataService.currentMessage.subscribe((message: any) => {
+        //             if (message != 'default message') {
+        //                 message = JSON.parse(message);
+        //                 if (message['type'] == 'return' && message['returnId'] == modalId) {
+        //                     if(message['handler']=='ok'){}
+        //                     // if(message['handler']=='cancel'){}
+        //                 }
+        //             }
+        //         });
+        //         return;
+        //     }
+        //     res = res.success;
+        //     let result =res['results'];
+        //     this.downloadedRecords = result;
+
+        //     this.utilService.generateCSV(result);
+        //     this.loaderService.hideLoader()
+        //     this.tableDisplay = this.generateTableService.getTableDisplay(this.downloadedRecords);
+        // },err=>{
+        //     this.loaderService.hideLoader();
+        //     this.utilService.showNetworkErrorModal()
+        // });
+    }catch(e){
+        // let modalId = this.utilService.generateModalId();
+        // this.dataService.changeMessage(JSON.stringify({
+        //     type: 'modal',
+        //     requestId: modalId,
+        //     modalType: "error",
+        //     buttonText: "Okay",
+        //     message: "Something wrong with given inputs, Please try again."
+        // }));
+    }
+
+
+}
 
 }
