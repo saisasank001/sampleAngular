9af2c4010f68a7552af14c1f1b67ac418afc9a35
anusha <anushavennela1@gmail.com>
2020-05-14 17:14:08 +0530

    announcement details display and user servie

--- src/app/announcement-details/announcement-details.component.ts
+++ src/app/announcement-details/announcement-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-announcement-details',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./announcement-details.component.scss']
 })
 export class AnnouncementDetailsComponent implements OnInit {
+ json:any ={
+  CreatedDate: "2020-05-09",
+ EndDate: "2020-06-22",
+ EndTime: "03:03:00",
+ IsAnnouncement: "1",
+ IsBanner: "1",
+ IsEmailSend: "0",
+ ModifiedDate: "5/11/2020 9:13:04 AM",
+ Priority: "1",
+ StartDate: "2020-06-20",
+ StartTime: "02:02:00",
+ announcement: "ZmluYWwgd2l0aG91dCBlbWFpbKA=",
+ announcement_id: "4692",
+ announcements: true,
+ endTime: {hour: 3, minute: 3, second: 0},
+ filename: "",
+ fromDate: {year: 2020, month: 6, day: 20},
+ message: "final without email ",
+ sendEmail: "0",
+ startTime: {hour: 2, minute: 2, second: 0},
+ title: "final without email ",
+ toDate: {year: 2020, month: 6, day: 22},
+ urgentMessage: true
+}
 
-  constructor() { }
+type ="Announcement"
+  constructor(public userService:UserService) { }
 
   ngOnInit() {
+    this.json.announcement = atob(this.json.announcement)
   }
 
 }
