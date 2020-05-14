f6bf8b771a4a66f1161aca644a9c41341639074c
anusha <anushavennela1@gmail.com>
2020-05-14 20:46:33 +0530

    active function still on testing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpServiceService } from '../http-service.service';
 
 @Component({
   selector: 'app-header',
@@ -6,17 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  url= 'products-house'
-  constructor() { }
+  url= '/products-/house'
+  name=''
+  constructor(public router:Router, public httpService:HttpServiceService) { }
 
   ngOnInit() {
+    this.router.events.subscribe(value => {
+      console.log('current route: ', this.router.url.toString());
+  });
   }
 
   checkActive(urlData){
-    if(urlData.includes(this.url)){
-
+    console.log("urlData",urlData)
+    console.log("urlData.includes(this.url)",this.url.includes(urlData))
+    if(this.url.includes(urlData)){
       return true
-      
     }
   }
 
